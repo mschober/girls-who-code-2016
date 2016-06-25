a846f3014ed49fa9a5ed306b8aba5c256e2cc8f2
Michael Schober <mschober@expedia.com>
2016-06-24 23:07:55 -0700

    matchers to respond to user

--- src/app.py
+++ src/app.py
@@ -1,3 +1,4 @@
+import re
 import time
 import os
 
@@ -33,41 +34,74 @@ HAL_H = 19
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
 
-def print_hal_letter(letter):
-    cls()
-    print letter
-    print
-    time.sleep(1)
-
-
 def read_hal():
     with open('src/hal.ascii', 'r') as hal_file:
         hal = hal_file.readlines()
     return hal
 
-def print_hal(hal_ascii, offset):
+def print_hal_letter(hal_ascii, offset):
     for line in range(len(hal_ascii)):
         slant_step = line #slant step is the amount of character increase as you go down the lines
         print (hal_ascii[line][:offset + slant_step])
 
-def print_hal_letter(hal, offset):
+def display_hal_letter(hal, offset):
     cls()
-    print_hal(hal, offset)
+    print_hal_letter(hal, offset)
     time.sleep(1)
+
+
+def print_hal():
+    display_hal_letter(hal, HAL_H)
+    display_hal_letter(hal, HAL_HA)
+    display_hal_letter(hal, HAL_HAL)
+    display_hal_letter(hal, HAL_HAL__)
+    display_hal_letter(hal, HAL_HAL__5)
+    display_hal_letter(hal, HAL_HAL_50)
+    display_hal_letter(hal, HAL_HAL_500)
+    display_hal_letter(hal, HAL_HAL_5000)
+    display_hal_letter(hal, HAL_HAL_5000_DOT)
+    display_hal_letter(hal, HAL_HAL_5000_DOT_DOT)
+    display_hal_letter(hal, HAL_HAL_5000_DOT_DOT_DOT)
+
+
+def handle_user_input(user_input):
+    user_input = clean_user_input(user_input)
+    salutations_pattern = re.compile(".*hi.*|.*hello.*")
+    addressing_hal_pattern = re.compile(".*hal.*")
+    open_ended_question_pattern = re.compile(".*what.*")
+    confused_pattern = re.compile(".*confused.*|.*lost.*")
+    books_pattern = re.compile(".*book.*")
+    inquiring_about_hal_pattern = re.compile(".*who.*you.*")
+
+    if user_input == '':
+        print 'you seem confused'
+    elif books_pattern.match(user_input):
+        print 'explain about the books'
+    elif addressing_hal_pattern.match(user_input):
+        print 'some reply from hal'
+    elif salutations_pattern.match(user_input):
+        print 'something about hi'
+    elif open_ended_question_pattern.match(user_input):
+        print 'you need some guidance'
+    elif confused_pattern.match(user_input):
+        print 'its easy to get lost'
+    elif inquiring_about_hal_pattern.match(user_input):
+        print 'hals an important scary guys'
+
+
+def clean_user_input(user_input):
+    user_input = user_input.strip()
+    user_input = user_input.lower()
+    return user_input
+
 
 if __name__ == '__main__':
     hal = read_hal()
-    print_hal_letter(hal, HAL_H)
-    print_hal_letter(hal, HAL_HA)
-    print_hal_letter(hal, HAL_HAL)
-    print_hal_letter(hal, HAL_HAL__)
-    print_hal_letter(hal, HAL_HAL__5)
-    print_hal_letter(hal, HAL_HAL_50)
-    print_hal_letter(hal, HAL_HAL_500)
-    print_hal_letter(hal, HAL_HAL_5000)
-    print_hal_letter(hal, HAL_HAL_5000_DOT)
-    print_hal_letter(hal, HAL_HAL_5000_DOT_DOT)
-    print_hal_letter(hal, HAL_HAL_5000_DOT_DOT_DOT)
+    # print_hal()
+    display_hal_letter(hal, HAL_HAL_5000_DOT_DOT_DOT)
+
     print
-    user_input = raw_input('> ')
-    print user_input
+    while True:
+        user_input = raw_input('> ')
+        handle_user_input(user_input)
+
